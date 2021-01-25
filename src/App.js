741cf7d052129398c42f9357d7afbc45e35ffd3f
mukaofssn <mukaofssn@gmail.com>
2021-01-25 18:35:53 +0000

    Cleanup.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-items App-header">				
+				<h3>
+					BetLight
+				</h3>			
+			</header>
+      <div className="App-items App-content"></div>
+      <footer className="App-items App-footer">
+      <p className="powered-by"><img src={logo} className="App-logo" alt="logo" /></p>
+      </footer>
+		</div>
+	);
 }
 
 export default App;
