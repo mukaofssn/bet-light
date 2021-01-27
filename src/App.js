ffc69fc21dbed5dd609e4ea96bddddb09e6ef523
mukaofssn <mukaofssn@gmail.com>
2021-01-27 17:14:02 +0000

    More clean up.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<header className="App-items App-header">				
-				<h3>
-					BetLight
-				</h3>			
+			<header className="App-items App-header">
+				<h3>BetLight</h3>
 			</header>
-      <div className="App-items App-content"></div>
-      <footer className="App-items App-footer">
-      <p className="powered-by"><img src={logo} className="App-logo" alt="logo" /></p>
-      </footer>
+			<div className="App-items App-content" />
+			<footer className="App-items App-footer">
+				<p className="powered-by">
+					<img src={logo} className="App-logo" alt="logo" />
+				</p>
+			</footer>
 		</div>
 	);
 }
