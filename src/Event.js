49180e7991fc8d7e02913caf7ab521b571860720
mukaofssn <mukaofssn@gmail.com>
2021-01-28 22:47:24 +0000

    Moved web socket logic into a custom hook.

--- src/Event.js
+++ src/Event.js
@@ -1,45 +1,26 @@
-import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
 
 const Event = ({ event, markets }) => {
-	const [ marketInfo, setMarketInfo ] = useState('');
-
-	useEffect(() => {
-		if (markets) {
-			// console.log(markets);
-			let ws = new WebSocket('ws://localhost:8889');
-			ws.onmessage = (e) => {
-				const result = JSON.parse(e.data);
-				if (result.type === 'MARKET_DATA') {
-					// console.log(result);
-					setMarketInfo(`${result.data.name} (${result.data.type})`);
-				}
-			};
-			ws.onopen = () => {
-				// console.log('Getting market data for ' + markets);
-				ws.send(
-					JSON.stringify({
-						type: 'getMarket',
-						id: markets
-					})
-				);
-			};
-			ws.onclose = () => {
-				// console.log('ws closed');
-			};
-
-			return () => {
-				if (ws.readyState === 1) {
-					ws.send(JSON.stringify({ type: 'unsubscribe' }));
-					ws.close();
-				}
-			};
-		}
-	});
+	const { data: marketsResult, isPending, error } = useFetch(
+		'MARKET_DATA',
+		markets,
+		JSON.stringify({
+			type: 'getMarket',
+			id: markets
+		})
+	);
 
 	return (
 		<div className="event-preview">
 			<p>{event.name}</p>
-			<p>{markets ? marketInfo : ""}</p>
+			<p>{markets && isPending ? 'Loading markets data..' : ''}</p>
+			<p>
+				{markets && !isPending && !error ? (
+					`${marketsResult.name} (${marketsResult.type})`
+				) : (
+					''
+				)}
+			</p>
 		</div>
 	);
 };
--- src/Events.js
+++ src/Events.js
@@ -1,13 +1,20 @@
-import Event from "./Event";
+import Event from './Event';
 
-const Events = ({events, showPrimary}) => {    
-    return ( 
-        events && events.map((event, id) => {            
-            return (
-                <Event event={event} key={id} markets={showPrimary && event.markets ? event.markets[0] : null} />
-            );
-        })
-     );
-}
- 
+const Events = ({ events, showPrimary }) => {
+	return (
+		events &&
+		events.map((event, id) => {
+			return (
+				<Event
+					event={event}
+					key={id}
+					markets={
+						showPrimary && event.markets ? event.markets[0] : null
+					}					
+				/>
+			);
+		})
+	);
+};
+
 export default Events;
