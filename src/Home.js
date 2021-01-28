9af031f331771fc50fa3968550e4eb493dbec179
mukaofssn <mukaofssn@gmail.com>
2021-01-28 22:47:24 +0000

    Moved web socket logic into a custom hook.

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,15 @@
-import { useState, useEffect } from 'react';
 import Events from './Events';
 import './Home.css';
+import useFetch from './useFetch';
 
 const Home = ({ primaryMarkets }) => {
-	const [ liveEvents, setLiveEvents ] = useState(null);
-	const [ isPending, setIsPending ] = useState(true);
-	const [ error, setError ] = useState(null);
-
-	useEffect(
-		() => {
-			let ws = new WebSocket('ws://localhost:8889');
-			ws.onmessage = (e) => {
-				const result = JSON.parse(e.data);
-				// console.log(result);
-				if (result.type === 'LIVE_EVENTS_DATA') {
-					setLiveEvents(result.data);
-					setIsPending(false);
-					setError(false);
-				}
-			};
-			ws.onopen = () => {
-				// console.log('ws opened');
-				// console.log('Sending with primary markets = ' + primaryMarkets);
-				ws.send(
-					JSON.stringify({
-						type: 'getLiveEvents',
-						primaryMarkets: primaryMarkets
-					})
-				);
-			};
-			ws.onclose = () => {
-				// console.log('ws closed');
-			};
-
-			return () => {
-				if (ws.readyState === 1) {
-					ws.send(JSON.stringify({ type: 'unsubscribe' }));
-					ws.close();
-				}
-			};
-		},
-		[ primaryMarkets ]
+	const { data: liveEvents, isPending, error } = useFetch(
+		'LIVE_EVENTS_DATA',
+		true,
+		JSON.stringify({
+			type: 'getLiveEvents',
+			primaryMarkets: primaryMarkets
+		})
 	);
 
 	return (
@@ -48,7 +17,7 @@ const Home = ({ primaryMarkets }) => {
 			<div className="encloseEvents">
 				{error && <h2>{error}</h2>}
 				{isPending && <h2>Loading...</h2>}
-				<Events events={liveEvents} showPrimary={primaryMarkets} />
+				{liveEvents && <Events events={liveEvents} showPrimary={primaryMarkets} />}
 			</div>
 		</div>
 	);
---
+++ src/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (type, trigger, request) => {
+	const [ data, setData ] = useState(null);
+	const [ isPending, setIsPending ] = useState(true);
+	const [ error, setError ] = useState(null);
+
+	useEffect(
+		() => {			
+			if (!trigger || trigger === undefined) return { data: null, isPending: false, error: null };
+			let ws = new WebSocket('ws://localhost:8889');
+			ws.onmessage = (e) => {
+				const result = JSON.parse(e.data);
+				if (result.type === type) {
+					console.log(result);
+					setData(result.data);
+					setIsPending(false);
+					setError(false);
+				}
+			};
+			ws.onopen = () => {                
+                console.log(type, trigger, request);
+				ws.send(request);
+			};
+			ws.onclose = () => {
+				console.log('ws closed');
+			};
+
+			ws.onerror = (err) => {
+				console.log(err);
+				setError(err.message);
+				setIsPending(false);
+			};
+
+			return () => {
+				if (ws.readyState === 1) {
+					ws.send(JSON.stringify({ type: 'unsubscribe' }));
+					ws.close();
+				}
+			};
+		},
+		[ type, trigger, request ]
+	);
+
+	return { data, isPending, error };
+};
+
+export default useFetch;
